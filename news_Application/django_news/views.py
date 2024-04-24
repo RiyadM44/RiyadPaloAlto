44cbece1c1e393e9e0ebadfe6c170a5333d1db64
RiyadM44 <riyad.murad@techvitis.com>
2024-04-24 12:18:03 +0300

    views

--- news_Application/django_news/views.py
+++ news_Application/django_news/views.py
@@ -8,7 +8,7 @@ def save_text_input(request):
 def home(request):
     query = request.GET.get('query')  # Get the search query from the request
     api_key = 'fbc50f8083bb4dada1367e8345947f14'
-    url = f'https://newsapi.org/v2/everything?q={query}&from=2024-03-17&sortBy=publishedAt&apiKey={api_key}'  
+    url = f'https://newsapi.org/v2/everything?q={query}&from=2024-03-24&sortBy=publishedAt&apiKey={api_key}'  
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
